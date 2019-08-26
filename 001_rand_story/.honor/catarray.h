12ff56a1b7cf6648545a3fac5b0d7e38a5c7b759
sl603 <sl603@duke.edu>
2019-08-26 16:16:57 -0400

    Released 001_rand_story

---
+++ 001_rand_story/.honor/catarray.h
@@ -0,0 +1,15 @@
+#ifndef __CATARRAY_H__
+#define __CATARRAY_H__
+#include <stdlib.h>
+
+struct category_tag {
+  //Write code here for this struct
+};
+typedef struct category_tag category_t;
+
+struct catarray_tag {
+  //Write code here for this struct
+};
+typedef struct catarray_tag catarray_t;
+
+#endif
