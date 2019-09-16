fd0b68ba0f651bd043b31a5c2e36bf8924c0afee
sl603 <sl603@duke.edu>
2019-09-16 16:16:19 -0400

    Assignment 024 commit 1

---
+++ 024_expr_eval1/expr.h
@@ -0,0 +1,46 @@
+#include <sstream>
+#include <string>
+
+class Expression {
+ public:
+  virtual std::string toString() const = 0;
+  virtual ~Expression() {}
+};
+
+class NumExpression : public Expression {
+ protected:
+  long number;
+
+ public:
+  NumExpression(long num) : number(num) {}
+  std::string toString() const {
+    std::stringstream ss;
+    ss << number;
+    return ss.str();
+  }
+  virtual ~NumExpression() {}
+};
+
+class OperationExpression : public Expression {
+ protected:
+  Expression * lOperand;
+  Expression * rOperand;
+
+ public:
+  OperationExpression(Expression * lhs, Expression * rhs) :
+      lOperand(lhs),
+      rOperand(rhs) {}
+  virtual std::string toString() const = 0;
+  virtual ~OperationExpression() {}
+};
+
+class PlusExpression : public OperationExpression {
+ public:
+  PlusExpression(Expression * lhs, Expression * rhs) : OperationExpression(lhs, rhs) {}
+  std::string toString() const {
+    std::stringstream ss;
+    ss << "(" << lOperand->toString() << " + " << rOperand->toString() << ")";
+    return ss.str();
+  }
+  virtual ~PlusExpression() {}
+};
