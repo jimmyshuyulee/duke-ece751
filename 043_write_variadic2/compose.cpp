9a53939a05245dbcd86e3baf76f11348ae214109
sl603 <sl603@duke.edu>
2019-09-30 23:00:48 -0400

    commit 1

--- 043_write_variadic2/compose.cpp
+++ 043_write_variadic2/compose.cpp
@@ -3,18 +3,18 @@
 // TODO: Base case
 template<typename Ftype>
 Ftype compose(const Ftype & fn) {
-  return nullptr;
+  return fn;
 }
 
 // TODO: Recursive case
 template<typename Ftype, typename... Other>
 auto compose(Ftype fn, Other... other) {
-  return nullptr;
+  return [fn, other...](auto x) { return compose(other...)(fn(x)); };
 }
 
 int main(void) {
-  auto add_func = [] (const int x) { return x + 5; };
-  auto sub_func = [] (const int x) { return x - 3; };
+  auto add_func = [](const int x) { return x + 5; };
+  auto sub_func = [](const int x) { return x - 3; };
   int res = compose(add_func, sub_func)(0);
   std::cout << "Result: " << res << "\n";
 }
