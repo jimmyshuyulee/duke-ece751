57b17f19cfb9089db42b3834b036505572d16a11
sl603 <sl603@duke.edu>
2019-09-30 21:17:34 -0400

    commit 1

--- 042_write_variadic1/myPrint.cpp
+++ 042_write_variadic1/myPrint.cpp
@@ -1,14 +1,21 @@
+#include <cstring>
 #include <iostream>
-
 // We define a namespace so printf does not clash
 namespace practice {
 
   // TODO: This is your base function
-  void printf(const char * format) {}
+  void printf(const char * format) { std::cout << format; }
 
   // TODO: This is your 'recursive' function
   template<typename T, typename... Targs>
-  void printf(const char * format, T value, Targs... args) {}
+  void printf(const char * format, T value, Targs... args) {
+    const char * p = strchr(format, '$');
+    char temp[p - format + 1];
+    strncpy(temp, format, p - format);
+    temp[p - format] = '\0';
+    std::cout << temp << value;
+    printf(p + 1, args...);
+  }
 
 }  // namespace practice
 
