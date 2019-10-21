c6ebbf7ef5e1b405514e27053263c4246eafef25
sl603 <sl603@duke.edu>
2019-10-21 16:12:19 -0400

    commit 1

--- 059_huff_tree/buildTree.cpp
+++ 059_huff_tree/buildTree.cpp
@@ -1,5 +1,17 @@
 #include "node.h"
 
-Node *  buildTree(uint64_t * counts) {
+Node * buildTree(uint64_t * counts) {
   //WRITE ME!
-}  
+  priority_queue_t pq;
+  for (unsigned i = 0; i < 257; ++i) {
+    pq.push(new Node(i, counts[i]));
+  }
+  while (pq.size() > 1) {
+    Node * left = pq.top();
+    pq.pop();
+    Node * right = pq.top();
+    pq.pop();
+    pq.push(new Node(left, right));
+  }
+  return pq.top();
+}
