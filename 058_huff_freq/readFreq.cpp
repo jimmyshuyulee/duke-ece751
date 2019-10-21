5e735dda6822a31e7b86bcd23d30b3e7a21259ee
sl603 <sl603@duke.edu>
2019-10-21 15:33:36 -0400

    commit 1

--- 058_huff_freq/readFreq.cpp
+++ 058_huff_freq/readFreq.cpp
@@ -1,9 +1,14 @@
-#include <stdio.h>
-#include <cstdlib>
 #include "readFreq.h"
 
+#include <stdio.h>
+
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
 void printSym(std::ostream & s, unsigned sym) {
-  if (sym > 256 ) {
+  if (sym > 256) {
     s << "INV";
   }
   else if (sym == 256) {
@@ -21,4 +26,15 @@ void printSym(std::ostream & s, unsigned sym) {
 }
 uint64_t * readFrequencies(const char * fname) {
   //WRITE ME!
+  uint64_t * freq = new uint64_t[257]();
+  std::ifstream ifs;
+  ifs.open(fname);
+  int c = 0;
+  while ((c = ifs.get()) != EOF) {
+    ++freq[c];
+  }
+  freq[256] = 1;
+
+  ifs.close();
+  return freq;
 }
