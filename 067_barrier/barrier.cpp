8c8bf8559d040560348ddeb968f05e804d194852
sl603 <sl603@duke.edu>
2019-10-31 15:35:20 -0400

    commit 1

--- 067_barrier/barrier.cpp
+++ 067_barrier/barrier.cpp
@@ -7,5 +7,11 @@
  * TODO: Implement
  */
 bool Barrier::enter() {
+  busy_thread--;
+  while (busy_thread > 1) {
+    std::unique_lock<std::mutex> lck(mtx);
+    cv.wait(lck);
+  }
+  cv.notify_all();
   return true;
 }
--- 067_barrier/barrier.hpp
+++ 067_barrier/barrier.hpp
@@ -1,12 +1,19 @@
 #include <condition_variable>
 #include <mutex>
 
+using std::condition_variable;
+using std::mutex;
+
 class Barrier {
  private:
   // TODO: Fill in fields
+  mutex mtx;
+  condition_variable cv;
+  int busy_thread;
 
  public:
   // TODO: Constructor
+  Barrier(int n) : busy_thread(n) {}
   bool enter();
 };
 
