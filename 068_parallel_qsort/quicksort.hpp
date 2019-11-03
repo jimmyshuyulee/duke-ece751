fc07094904480e26d7a04cf5c96a9428bcb232ea
sl603 <sl603@duke.edu>
2019-11-03 11:43:15 -0500

    test new qucksort

--- 068_parallel_qsort/quicksort.hpp
+++ 068_parallel_qsort/quicksort.hpp
@@ -27,9 +27,10 @@ void quicksort(ECE751::ThreadPool<> & pool,
     }
   }
   auto left = [&pool, &vec, L, pivot, c] { quicksort(pool, vec, L, pivot - 1, c); };
-  auto right = [&pool, &vec, R, pivot, c] { quicksort(pool, vec, pivot + 1, R, c); };
+  //auto right = [&pool, &vec, R, pivot, c] { quicksort(pool, vec, pivot + 1, R, c); };
   pool.enqueue(left);
-  pool.enqueue(right);
+  //pool.enqueue(right);
+  quicksort(pool, vec, pivot + 1, R, c);
 }
 
 /*
