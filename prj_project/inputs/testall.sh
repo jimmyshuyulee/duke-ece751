42bbf16ad141666a3d02ff8c6f5f46c9afe0e7b7
sl603 <sl603@duke.edu>
2019-12-06 11:56:59 -0500

    added test cases

---
+++ prj_project/inputs/testall.sh
@@ -0,0 +1,5 @@
+../workflow run test1.txt ../logs/output1.txt
+../workflow run test2.txt ../logs/output2.txt
+../workflow run test3.txt ../logs/output3.txt
+../workflow run test4.txt ../logs/output4.txt
+../workflow run test5.txt ../logs/output5.txt
