b6dd0f17be3ebcb92c23defe62a39d6ed9fc0e8d
sl603 <sl603@duke.edu>
2019-12-08 21:40:08 -0500

    069 new version

--- 069_genetic_algorithm/ga.hpp
+++ 069_genetic_algorithm/ga.hpp
@@ -7,8 +7,8 @@
 #include <vector>
 
 #include "per_thread_random.hpp"
-#include "quicksort.hpp" // XXX 0: Must be symlinked
-#include "threadpool.hpp" // XXX 0: Must be symlinked
+#include "quicksort.hpp"   // XXX 0: Must be symlinked
+#include "threadpool.hpp"  // XXX 0: Must be symlinked
 
 /*
  * GeneticAlgorithm is templated. Fitness.cpp contains
@@ -30,21 +30,38 @@ class GeneticAlgorithm {
   std::vector<std::pair<Gene, double> > * next_gen;
 
  private:
-
   /*
    * This function should populate population1 with std::pairs 
    * containing a random gene and the fitness of that gene.
    * You should split work evenly across threads. 
    */
   void initialize_population(ECE751::ThreadPool<> & tp, const InitParams & ip) {
+    auto task = [ip, this](int lower, int upper) {
+      for (int j = lower; j < upper; j++) {
+        *prev_gen[j] = makeRandomGene(ip);
+      }
+    };
+    for (int i = 0; i < tp.size(); i++) {
+      if (i == tp.size() - 1) {
+        tp.runTaskToCompletion(task(i * (prev_gen->size() / tp.size()),
+                                    (i + 1) * (prev_gen->size() / tp.size())));
+      }
+      else {
+        tp.enqueue(task(i * (prev_gen->size() / tp.size()),
+                        (i + 1) * (prev_gen->size() / tp.size())));
+      }
+    }
   }
-
   /*
    * Sorts the given population. You should define a comparison function and 
    * pass it into parallel_qsort
    */
   void sort_pop(ECE751::ThreadPool<> & tp,
                 std::vector<std::pair<Gene, double> > * population) {
+    auto c = [](std::pair<Gene, double> a, std::pair<Gene, double> b) {
+      return a.second < b.second;
+    };
+    parallel_qsort(tp, population, c);
   }
 
   /*
@@ -54,6 +71,23 @@ class GeneticAlgorithm {
    * You should split work evenly across threads.
    */
   void breed_pop(ECE751::ThreadPool<> & tp) {
+    auto task = [this](int lower, int upper) {
+      for (int j = lower; j < upper; j++) {
+        int t1 = per_thread_random() % prev_gen->size();
+        int t2 = per_thread_random() % prev_gen->size();
+        *next_gen[j] = breed(*prev_gen[t1], *prev_gen[t2]);
+      }
+    };
+    for (int i = 0; i < tp.size(); i++) {
+      if (i == tp.size() - 1) {
+        tp.runTaskToCompletion(task(i * (next_gen->size() / tp.size()),
+                                    (i + 1) * (next_gen->size() / tp.size())));
+      }
+      else {
+        tp.enqueue(task(i * (next_gen->size() / tp.size()),
+                        (i + 1) * (next_gen->size() / tp.size())));
+      }
+    }
   }
 
  public:
@@ -74,9 +108,7 @@ class GeneticAlgorithm {
    *     prev and next generation.
    *  3. Sort the final result and return the resulting gene. 
    */
-  Gene run(ECE751::ThreadPool<> & tp, const InitParams & ip) {
-    return Gene();
-  }
+  Gene run(ECE751::ThreadPool<> & tp, const InitParams & ip) { return Gene(); }
 };
 
 #endif
