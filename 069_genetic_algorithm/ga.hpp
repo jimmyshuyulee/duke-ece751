2ae35ff001e58fab704661fb941351bb2bb4b1fb
sl603 <sl603@duke.edu>
2019-12-08 23:39:16 -0500

    test 069

--- 069_genetic_algorithm/ga.hpp
+++ 069_genetic_algorithm/ga.hpp
@@ -123,7 +123,7 @@ class GeneticAlgorithm {
     for (size_t i = 0; i < max_iter; i++) {
       sort_pop(tp, prev_gen);
       breed_pop(tp);
-      std::swap(prev_gen, next_gen);
+      std::swap(*prev_gen, *next_gen);
     }
     sort_pop(tp, next_gen);
     return (*next_gen)[next_gen->size() - 1].first;
